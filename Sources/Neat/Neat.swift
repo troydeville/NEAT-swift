7b90e1f7998ddd781f8f0842befa3114827aaebd
Troy Deville <troydeville@Troys-MBP.domain>
2018-05-08 00:31:20 -0500

    Made certain variables public

--- Sources/Neat/Neat.swift
+++ Sources/Neat/Neat.swift
@@ -7,19 +7,20 @@ public struct Neat {
     }
     
     // Network's inputs
-    var inputs = [[Double]]()
+    public var inputs = [[Double]]()
     
     // Network's Generation Identifier
-    var generation = 0
+    public var generation = 0
     
     // Network's King Genome and Fitness Score
-    var king = Genome()
-    var topFitnessScore = 0.0
+    public var king = Genome()
+    public var topFitnessScore = 0.0
     
     // Network's Population Control
-    var populationIndexPos: Int = 0
-    var currentFitnessScore = 0.0
+    public var populationIndexPos: Int = 0
+    public var currentFitnessScore = 0.0
     
+    // Run the genome
     public mutating func run(input: Double..., continuous: Bool, logs: Bool) -> [Double] {
         var depth: Int = 1
         var lowestPosition: Double = 1000
@@ -39,6 +40,16 @@ public struct Neat {
         let output = network.update(inputs: input, type: RunType.snapshot, genome: populationIndexPos)
         
         if !continuous {
+            
+            if currentFitnessScore > topFitnessScore {
+                topFitnessScore = currentFitnessScore
+                if logs { print("FITNESS: \(currentFitnessScore)") }
+                print(network.getGenome(index: populationIndexPos).toString())
+                let w = network.getGenome(index: populationIndexPos)
+                king = Genome(id: w.getID(), neurons: w.getNeuronGenes(), links: w.getLinkGenes(), inputs: w.getInputCount(), outputs: w.getOutputCount())
+                king.setFitness(fit: currentFitnessScore)
+            }
+            
             // Incriment the population's index position.
             self.network.setGenomeFitness(index: populationIndexPos, fitness: currentFitnessScore)
             self.currentFitnessScore = 0.0
