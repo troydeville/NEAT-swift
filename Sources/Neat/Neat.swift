75a6fd9e18efa22e446eb99d7ec060fe17603df5
Troy Deville <troydeville@Troys-MBP.domain>
2018-05-08 00:57:49 -0500

    Update to the functionality of the implementation.

--- Sources/Neat/Neat.swift
+++ Sources/Neat/Neat.swift
@@ -21,7 +21,7 @@ public struct Neat {
     public var currentFitnessScore = 0.0
     
     // Run the genome
-    public mutating func run(input: Double..., continuous: Bool, logs: Bool) -> [Double] {
+    public mutating func run(input: Double...) -> [Double] {
         var depth: Int = 1
         var lowestPosition: Double = 1000
         for n in network.genomeGetNeuronGenes(index: populationIndexPos) {
@@ -39,33 +39,33 @@ public struct Neat {
         // Output Result
         let output = network.update(inputs: input, type: RunType.snapshot, genome: populationIndexPos)
         
-        if !continuous {
-            
-            if currentFitnessScore > topFitnessScore {
-                topFitnessScore = currentFitnessScore
-                if logs { print("FITNESS: \(currentFitnessScore)") }
-                print(network.getGenome(index: populationIndexPos).toString())
-                let w = network.getGenome(index: populationIndexPos)
-                king = Genome(id: w.getID(), neurons: w.getNeuronGenes(), links: w.getLinkGenes(), inputs: w.getInputCount(), outputs: w.getOutputCount())
-                king.setFitness(fit: currentFitnessScore)
-            }
-            
-            // Incriment the population's index position.
-            self.network.setGenomeFitness(index: populationIndexPos, fitness: currentFitnessScore)
-            self.currentFitnessScore = 0.0
-            self.populationIndexPos += 1
-            if logs { print("Genome: \(populationIndexPos)") }
-            
-            if populationIndexPos == network.populationCount() {
-                network.epoch()
-                generation += 1
-                populationIndexPos = 0
-                if logs { print("Generation: \(generation)") }
-            }
-            
-        }
-        
         return output
+    }
+    
+    public mutating func compareFitness(logs: Bool) {
+        if currentFitnessScore > topFitnessScore {
+            topFitnessScore = currentFitnessScore
+            if logs { print("FITNESS: \(currentFitnessScore)") }
+            if logs { print(network.getGenome(index: populationIndexPos).toString()) }
+            let w = network.getGenome(index: populationIndexPos)
+            king = Genome(id: w.getID(), neurons: w.getNeuronGenes(), links: w.getLinkGenes(), inputs: w.getInputCount(), outputs: w.getOutputCount())
+            king.setFitness(fit: currentFitnessScore)
+        }
+    }
+    
+    public mutating func runNext(logs: Bool) {
+        // Incriment the population's index position.
+        self.network.setGenomeFitness(index: populationIndexPos, fitness: currentFitnessScore)
+        self.currentFitnessScore = 0.0
+        self.populationIndexPos += 1
+        //if logs { print("Genome: \(populationIndexPos)") }
+        
+        if populationIndexPos == network.populationCount() {
+            network.epoch()
+            generation += 1
+            populationIndexPos = 0
+            if logs { print("Generation: \(generation)") }
+        }
     }
     
 }
